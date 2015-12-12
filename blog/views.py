4d79d685df371de9158bdd53049408660f8d3216
vallllllll2000 <valerie.cardoso@gmail.com>
2015-12-12 13:28:09 +0100

    added first template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import Post
 
 def post_list(request):
     #return HttpResponseNotFound('<h1>Page not found</h1>')
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
